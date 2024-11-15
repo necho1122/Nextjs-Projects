1dd249804f422c4c6e9a168d78952d3eb7927221
necho1122 <nelsonubac90@gmail.com>
2024-11-14 22:39:03 -0300

    stiles update

--- lineal-bingo/lineal-bingo/components/BingoCell.jsx
+++ lineal-bingo/lineal-bingo/components/BingoCell.jsx
@@ -1,20 +1,27 @@
-// components/BingoCell.js
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 export function BingoCell({ cell, isWinning, isActive, styles }) {
-	return (
-		<div
-			className={
-				isWinning && isActive
-					? styles.winningActiveCell
-					: isActive
-					? styles.activeCell
-					: styles.cell
-			}
-		>
-			{cell}
-		</div>
-	);
+	const [isEnlarged, setIsEnlarged] = useState(false);
+
+	useEffect(() => {
+		if (isActive) {
+			setIsEnlarged(true); // Activa la animación
+			const timer = setTimeout(() => {
+				setIsEnlarged(false); // La desactiva después de 500 ms
+			}, 500); // Duración de la animación debe coincidir con el CSS
+
+			return () => clearTimeout(timer); // Limpia el temporizador al desmontar
+		}
+	}, [isActive]);
+
+	const cellClassName =
+		isWinning && isActive
+			? `${styles.winningActiveCell} ${isEnlarged ? styles.enlarge : ''}`
+			: isActive
+			? `${styles.activeCell} ${isEnlarged ? styles.enlarge : ''}`
+			: styles.cell;
+
+	return <div className={cellClassName}>{cell}</div>;
 }
 
 export default BingoCell;
